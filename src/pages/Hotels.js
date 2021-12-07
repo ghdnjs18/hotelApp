a98de118f1d25360ce5fda1eca17fea5a1f167c5
ghdnjs18 <ghdnjs18@naver.com>
2021-12-07 12:39:00 +0900

    지도 라이브러리를 이용한 호텔 위치 출력

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import "./Hotels.css";
 
-import { fetchHotelsCom, isArrayNull } from "lib";
+import { fetchHotelsCom, isArrayNull, handleNullObj } from "lib";
 import hotelsData from "../hotelsData";
+import { HotelItem } from "components";
 
 const Hotels = () => {
   const location = useLocation();
-  const { destinationId, checkIn, checkOut, adultsNumber } = location.state;
-  console.log(destinationId, checkIn, checkOut, adultsNumber);
+  const { destinationId, checkIn, checkOut, adultsNumber } =
+    location.state || {};
+  // console.log(destinationId, checkIn, checkOut, adultsNumber);
 
-  useEffect(() => {
-    getHotels();
-  });
+  const [hotels, setHotels] = useState([]);
+  const [mapObj, setMapObj] = useState(null); // 지도 객체를 저장할 state값
+
+  useEffect(async () => {
+    const hotelsList = await getHotels();
+    setHotels(hotelsList);
+
+    const m = L.map("map");
+    setMapObj(m);
+  }, []);
 
   const getHotels = async () => {
     // 실제 API로 가져올 때
@@ -23,11 +33,40 @@ const Hotels = () => {
     const {
       searchResults: { results },
     } = hotelsData;
-    console.log(results);
 
     return results;
   };
-  return <div>Hotels Page</div>;
+  const displayLocation = (lat, lon, msg) => {
+    console.log("지도 객체", mapObj);
+
+    if (mapObj) {
+      const map = mapObj.setView([lat, lon], 13);
+
+      // 지도의 배경화면
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // 지도에 마커 추가하기
+      L.marker([lat, lon]).addTo(map).bindPopup(msg).openPopup();
+    }
+  };
+  return (
+    <div className="Hotels-container">
+      <div id="map"></div>
+      {!isArrayNull(hotels) &&
+        hotels.map((hotel) => {
+          const { name, address, coordinate } = handleNullObj(hotel);
+          const { streetAddress, locality, countryName } =
+            handleNullObj(address);
+          const { lat, lon } = handleNullObj(coordinate);
+          const msg = `${name}<br>${streetAddress}, ${locality}, ${countryName}`;
+          displayLocation(lat, lon, msg);
+          return <HotelItem hotel={hotel} key={hotel.id} />;
+        })}
+    </div>
+  );
 };
 
 export default Hotels;
