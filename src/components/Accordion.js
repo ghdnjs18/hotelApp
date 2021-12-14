c9ac92ae5d3cd6726650c8ec9adb1a71663bf489
ghdnjs18 <ghdnjs18@naver.com>
2021-12-14 10:18:57 +0900

    accordionItem 출력 및 css 적용

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { isArrayNull } from "lib";
+
+import "Accordion.css";
 
 const Accordion = ({ title, items, displayFilter }) => {
   return (
     <div className="Accordion-container">
       <div className="Accordion-menu" onClick={displayFilter}>
-        <div className="Accordion-arrow">{items}</div>
+        <div className="Accordion-arrow"></div>
         <div className="Accordion-title">{title}</div>
+      </div>
+      <div className="Accordion-items">
+        {isArrayNull(items) &&
+          items.map((item) => {
+            return <AccordionItem key={item.value}>{item.label}</AccordionItem>;
+          })}
       </div>
     </div>
   );
